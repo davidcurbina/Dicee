346ff887c359f41606fc4ff7399b85d5be066deb
David Urbina <davidcurbina@gmail.com>
2017-11-30 19:53:35 -0500

    Added motion gesture shake feature, Added animation for shaking

--- Dicee/MainViewController.swift
+++ Dicee/MainViewController.swift
@@ -12,6 +12,7 @@ class MainViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    var rolls : Int = 0
     let imageArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
@@ -19,6 +20,7 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        shakeDice(count:1)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,13 +30,33 @@ class MainViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
         //Random Number generation, comes with UIKit
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex2 = Int(arc4random_uniform(6))
-        
-        diceImageView1.image = UIImage(named: imageArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: imageArray[randomDiceIndex1])
+        rolls = 4
+        for index in 1...rolls
+        {
+            print("Shake \(index)")
+            shakeDice(count:6)
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        shakeDice(count: 7)
+    }
+    
+    func shakeDice(count: Int){
+        if (count > 0)
+        {
+            //Add delay for animation
+            let when = DispatchTime.now() + 0.35
+            DispatchQueue.main.asyncAfter(deadline: when) {
+                self.randomDiceIndex1 = Int(arc4random_uniform(6))
+                self.randomDiceIndex2 = Int(arc4random_uniform(6))
+                
+                self.diceImageView1.image = UIImage(named: self.imageArray[self.randomDiceIndex1])
+                self.diceImageView2.image = UIImage(named: self.imageArray[self.randomDiceIndex2])
+                self.shakeDice(count:count-1)
+            }
+        }
     }
 }
 
